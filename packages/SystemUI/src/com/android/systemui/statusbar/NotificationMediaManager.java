71503b906ffe234f00bd6e4993dd3eee594cd10c
Chet Kener <Cl3Kener@gmail.com>
2020-08-22 14:50:52 +1000

    Speed up Notification Backdrop animation
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I394772b1492c81bf6992a0129f11ce8090476601

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -583,7 +583,7 @@ public class NotificationMediaManager implements Dumpable {
                                 + mBackdropBack.getDrawable());
                     }
                     mBackdropFront.animate()
-                            .setDuration(250)
+                            .setDuration(150)
                             .alpha(0f).withEndAction(mHideBackdropFront);
                 }
             }
