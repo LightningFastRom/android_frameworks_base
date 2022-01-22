d245ff5c4bb2734c80bef0dc6b459ee14385c280
Kevin F. Haggerty <haggertk@lineageos.org>
2022-01-22 09:36:57 -0700

    Merge tag 'android-security-10.0.0_r62' into staging/lineage-17.1_merge-android-security-10.0.0_r62
    Android security 10.0.0 release 62
    
    * tag 'android-security-10.0.0_r62':
      Force-set a ClipData to prevent later migration.
      Allow forcing status bar state changes and do so when the screen turns off.
      Revert "Revert "[pm] remove old stage dirs on low storage""
      Prevent apps from spamming addAccountExplicitly. See comment here for the discussion on solution https://b.corp.google.com/issues/169762606#comment14
      Prevent apps from spamming addAccountExplicitly. See comment here for the discussion on solution https://b.corp.google.com/issues/169762606#comment14
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
    
    Change-Id: I33a7768393f85f6f264b09d677e3e22ba8bc44a8
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3216,7 +3216,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 showKeyguardImpl();
             }
         } else {
-            return hideKeyguardImpl();
+            return hideKeyguardImpl(force);
         }
         return false;
     }
@@ -3358,11 +3358,11 @@ public class StatusBar extends SystemUI implements DemoMode,
     /**
      * @return true if we would like to stay in the shade, false if it should go away entirely
      */
-    public boolean hideKeyguardImpl() {
+    public boolean hideKeyguardImpl(boolean force) {
         mIsKeyguard = false;
         Trace.beginSection("StatusBar#hideKeyguard");
         boolean staying = mStatusBarStateController.leaveOpenOnKeyguardHide();
-        if (!(mStatusBarStateController.setState(StatusBarState.SHADE))) {
+        if (!(mStatusBarStateController.setState(StatusBarState.SHADE, force))) {
             //TODO: StatusBarStateController should probably know about hiding the keyguard and
             // notify listeners.
 
@@ -3860,7 +3860,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 // is correct.
                 mHandler.post(() -> onCameraLaunchGestureDetected(mLastCameraLaunchSource));
             }
-            updateIsKeyguard();
+            // When finished going to sleep, force the status bar state to avoid stale state.
+            updateIsKeyguard(true /* force */);
         }
 
         @Override
